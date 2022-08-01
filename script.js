70c2ed953ccd61dc599947a7f030ba0eb8856df7
seintpl <95775405+seintpl@users.noreply.github.com>
2022-08-01 21:13:30 +0200

    Horizontal/vertical pattern movement added

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-var loadFile = function(event) {
+var loadFile = function() {
 	var photo = document.getElementById('tresult');
 	photo.src = URL.createObjectURL(event.target.files[0]);
 };
+
+var tilth = document.getElementById("tiltv");
+tilth.oninput = function() {
+  document.getElementById("pattern").style.marginLeft = this.value + "px";
+}
+
+var tiltv = document.getElementById("tilth");
+tiltv.oninput = function() {
+  document.getElementById("pattern").style.marginTop = this.value + "px";
+}
